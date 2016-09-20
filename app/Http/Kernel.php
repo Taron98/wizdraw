6a1e4d58a4d800431cc4ae886010a6b2425f7c26
Eran Shmil <eran.php@gmail.com>
2016-09-20 16:24:57 +0300

    install laravel-cors package and implement in our project

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'cors',
         ],
 
         'auth' => [
@@ -58,5 +59,6 @@ class Kernel extends HttpKernel
         'throttle'    => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'jwt.auth'    => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+        'cors'        => \Barryvdh\Cors\HandleCors::class,
     ];
 }
