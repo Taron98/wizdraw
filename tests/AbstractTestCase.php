0d356693c7696bf27a64d4bb292be268bc4b51a2
Eran Shmil <eran.php@gmail.com>
2016-09-21 23:39:00 +0300

    just because...

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase;
 
+/**
+ * Class AbstractTestCase
+ * @package Wizdraw\Tests
+ */
 abstract class AbstractTestCase extends TestCase
 {
     use DatabaseMigrations, DatabaseTransactions;
