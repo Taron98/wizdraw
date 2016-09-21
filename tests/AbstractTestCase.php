c9847a4802baf5e5846f7cfbbae5041531a57078
Eran Shmil <eran.php@gmail.com>
2016-09-21 23:20:42 +0300

    add basic configuration for testing

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wizdraw\Tests;
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\TestCase;
+
+abstract class AbstractTestCase extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+}
