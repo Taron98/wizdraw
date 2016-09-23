f7871adfc5a1738ceee84717c9cafa2dfde37887
Eran Shmil <eran.php@gmail.com>
2016-09-23 15:44:24 +0300

    turn $data in database testing into snake cased

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -36,4 +36,32 @@ abstract class AbstractTestCase extends TestCase
         return $app;
     }
 
+    /**
+     * Assert that a given where condition exists in the database
+     *
+     * @param string $table
+     * @param array $data
+     * @param null $connection
+     *
+     * @return TestCase|AbstractTestCase
+     */
+    protected function seeInDatabase($table, array $data, $connection = null) : AbstractTestCase
+    {
+        return parent::seeInDatabase($table, array_key_snake_case($data), $connection);
+    }
+
+    /**
+     * Assert that a given where condition does not exist in the database
+     *
+     * @param string $table
+     * @param array $data
+     * @param null $connection
+     *
+     * @return TestCase|AbstractTestCase
+     */
+    protected function notSeeInDatabase($table, array $data, $connection = null) : AbstractTestCase
+    {
+        return parent::notSeeInDatabase($table, array_key_snake_case($data), $connection);
+    }
+
 }
