b37d89d8310797be7f56caa60d158d8ab5f7453e
Eran Shmil <eran.php@gmail.com>
2016-09-23 14:31:30 +0300

    change name attribute in group factory

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -2,7 +2,7 @@
 
 $factory->define(Wizdraw\Models\Group::class, function (Faker\Generator $faker) {
     return [
-        'name'            => $faker->firstName,
+        'name'            => $faker->lastName,
         'admin_client_id' => Wizdraw\Models\Client::all()->random()->id,
     ];
 });
