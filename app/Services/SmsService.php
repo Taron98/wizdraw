aa55f122020ac67540b785224c859567b8f97f3b
Restart1 <eladh@restrtit.co.il>
2016-10-19 18:15:34 +0300

    SMS new service

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -17,7 +17,7 @@ class SmsService extends AbstractService
 
     public function sendSms(string $phone,string $verifyCode){
         $ch = curl_init();
-        $string = 'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text=check this code-'. $verifyCode . '&from=+972537916395&to=' . $phone;
+        $string = 'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text=check%20this%20code-'. $verifyCode . '&from=+972537916395&to=' . $phone;
         print_r($string);
         curl_setopt($ch, CURLOPT_URL,$string);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
