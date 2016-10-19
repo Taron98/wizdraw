f5e090a41a46af703b5ea3cea36f440f1f85299c
Restart1 <eladh@restrtit.co.il>
2016-10-19 17:09:30 +0300

    check sms

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -18,7 +18,14 @@ class SmsService extends AbstractService
     public function sendSms(string $phone){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text=TEST%20TEST%20ETSS!&from=+972537916395&to=+972542223839');
-        $server_output = curl_exec ($ch);
+        if(curl_exec($ch) === false)
+        {
+            echo 'Curl error: ' . curl_error($ch);
+        }
+        else
+        {
+            echo 'Operation completed without any errors';
+        }
         curl_close ($ch);
 
     }
