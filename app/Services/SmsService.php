5a5f4d590733f5f9bbe51ebafda606c1a0d7ecee
Restart1 <eladh@restrtit.co.il>
2016-10-19 18:12:43 +0300

    SMS new service

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -17,17 +17,19 @@ class SmsService extends AbstractService
 
     public function sendSms(string $phone,string $verifyCode){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text=check this code '. $verifyCode . '&from=+972537916395&to=' . $phone);
         $string = 'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text=check this code '. $verifyCode . '&from=+972537916395&to=' . $phone;
         print_r($string);
+        curl_setopt($ch, CURLOPT_URL,$string);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-
-        if(curl_exec($ch) === false)
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        if($response === false)
         {
             echo 'Curl error: ' . curl_error($ch);
         }
         else
         {
+            print_r($response);
             echo 'Operation completed without any errors';
         }
         curl_close ($ch);
