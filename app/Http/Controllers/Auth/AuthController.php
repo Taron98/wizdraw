4a60b886e30a94ea7da5beec83b2067b1d17abb1
Restart1 <eladh@restrtit.co.il>
2016-10-20 10:26:19 +0300

    SMS new service

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -133,8 +133,10 @@ class AuthController extends AbstractController
         }
 
         // todo: relocation?
-        $this->smsService->sendSms($clientAttrs[ 'phone' ], $user[ 'verify_code' ]);
-
+        $sms = $this->smsService->sendSms($clientAttrs[ 'phone' ], $user[ 'verify_code' ]);
+        if (!$sms) {
+            return $this->respondWithError('problem sending SMS');
+        }
         return $this->respond([
             'token' => $this->authService->createTokenFromUser($user),
         ]);
--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -18,22 +18,20 @@ class SmsService extends AbstractService
     public function sendSms(string $phone,string $verifyCode){
         $ch = curl_init();
         $string = 'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text=check%20this%20code-'. $verifyCode . '&from=+972537916395&to=' . $phone;
-        print_r($string);
         curl_setopt($ch, CURLOPT_URL,$string);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         if($response === false)
         {
-            echo 'Curl error: ' . curl_error($ch);
+            $response =  false;
         }
         else
         {
-            print_r($response);
-            echo 'Operation completed without any errors';
+            $response =  true;
         }
         curl_close ($ch);
-
+        return $response;
     }
 
 }
