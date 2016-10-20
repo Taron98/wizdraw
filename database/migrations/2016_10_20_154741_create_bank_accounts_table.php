1a671a97fca1c02e90047eb1090bbc31308419f0
Eran Shmil <eran.php@gmail.com>
2016-10-20 19:18:22 +0300

    migrate bank_account table

---
+++ database/migrations/2016_10_20_154741_create_bank_accounts_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('bank_id')->unsigned()->index();
+            $table->integer('bank_branch_id')->unsigned()->index();
+            $table->integer('client_id')->unsigned()->index();
+            $table->string('account_number', 60)->nullable();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->softDeletes();
+
+            $table->foreign('bank_branch_id')
+                ->references('id')->on('bank_branches')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('client_id')
+                ->references('id')->on('clients')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
