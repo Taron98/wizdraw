73df3292fb9680e18a7bb16d8af0dc9f896f5653
Restart1 <eladh@restrtit.co.il>
2016-11-07 19:25:31 +0200

    SMS new service

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -19,7 +19,7 @@ class SmsService extends AbstractService
     {
         $phone = '+' . preg_replace('/[^0-9]/', '', $phone);
         $text = 'You have successfully granted access to wizdraw!Simply return to wizdraw and enter PIN to complete the process. activation code:' . $verifyCode;
-        $text = str_replace(" ","%",$text);
+        $text = urlencode($text);
         $ch = curl_init();
         $string = 'https://188.138.96.222/VSServices/SendSms.ashx?login=1258965269888&pass=Test$WF@01!&text='.  $text . '&from=Wizdraw&to=' . $phone;
         curl_setopt($ch, CURLOPT_URL, $string);
