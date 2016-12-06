86acd3b980023c0723f47a95c59bcc0ba42b6287
Eran Shmil <eran.php@gmail.com>
2016-12-06 11:53:48 +0200

    fix for newline in sms texts

--- resources/lang/en/sms.php
+++ resources/lang/en/sms.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'verification_first_time' => 'Your Wizdraw verification code is :verifyCode.\nSimply open the app and enter the code to complete the process.\nThis code is valid for :expireInMinutes hours.',
-    'verification'            => 'Your verification code is {$verifyCode}.',
+    'verification_first_time' => "Your Wizdraw verification code is :verifyCode.\nSimply open the app and enter the code to complete the process.\nThis code is valid for :expireInMinutes hours.",
+    'verification'            => "Your verification code is :verifyCode.",
 
     'transfer_receiver_pick_up' => ":receiverName\nsent you funds that are waiting for you to pick up. Your transaction number is :transactionNumber. Open the Wizdraw app for more details.",
     'transfer_receiver_deposit' => ":receiverName\ndeposited funds for you in your bank account. Your transaction number is :transactionNumber. Open the Wizdraw app for more details.",
