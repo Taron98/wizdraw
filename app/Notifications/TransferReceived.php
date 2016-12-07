92623c681e7300c11c9862389b5ec02ac9c9b9eb
Eran Shmil <eran.php@gmail.com>
2016-12-07 21:25:56 +0200

    add sms notifications for transfer's client receiver

---
+++ app/Notifications/TransferReceived.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Wizdraw\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Wizdraw\Models\Transfer;
+use Wizdraw\Models\TransferType;
+use Wizdraw\Models\User;
+use Wizdraw\Notifications\Channels\SmsChannel;
+use Wizdraw\Notifications\Messages\SmsMessage;
+
+/**
+ * Class TransferReceived
+ * @package Wizdraw\Notifications
+ */
+class TransferReceived extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /** @var  Transfer */
+    protected $transfer;
+
+    /**
+     * TransferMissingReceipt constructor.
+     *
+     * @param Transfer $transfer
+     */
+    public function __construct(Transfer $transfer)
+    {
+        $this->transfer = $transfer;
+    }
+
+    /**
+     * Get the notification channels.
+     *
+     * @param  mixed $notifiable
+     *
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return [SmsChannel::class];
+    }
+
+    /**
+     * @param User $notifiable
+     *
+     * @return SmsMessage
+     */
+    public function toSms(User $notifiable)
+    {
+        $attributes = [
+            'senderName'        => $this->transfer->client->getFullName(),
+            'transactionNumber' => $this->transfer->getTransactionNumber(),
+        ];
+
+        if ($this->transfer->type->getType() === TransferType::TYPE_PICKUP_CASH) {
+            $text = trans('sms.transfer_receiver_pick_up', $attributes);
+        } else {
+            $text = trans('sms.transfer_receiver_deposit', $attributes);
+        }
+
+        return SmsMessage::create()
+            ->setText($text);
+    }
+
+}
