d01ff2fa914e17f06856683109566df9af7f00b3
Zlil Korman <zlilk@restartit.co.il>
2017-01-11 16:10:04 +0200

    * reset password feature * reset route * reset endpoint at user controller * create email reset notification * create email reset notification view

---
+++ database/migrations/2017_01_11_160059_alter_feedback_question_id_change_to_nullable.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterFeedbackQuestionIdChangeToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feedbacks', function (Blueprint $table) {
+            $table->integer('feedback_question_id')->unsigned()->index()->nullable()->change();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
