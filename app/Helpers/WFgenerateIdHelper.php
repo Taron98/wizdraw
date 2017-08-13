af315e303090df44c05b863f19681972c4d281e2
Zlil Korman <zlilk@restartit.co.il>
2017-08-13 13:19:58 +0300

    * generate unique WF ID from WIC system

---
+++ app/Helpers/WFgenerateIdHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+
+const PASSWORD = 'WiZ32#WIC';
+const USER = 'WFMobileWiz';
+
+if (!function_exists('getWfId')) {
+    function getWfId()
+    {
+
+        // set post fields
+        $post = [
+            'username' => USER,
+            'password' => PASSWORD,
+            'method'   => 'getIdForWizdraw',
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL,"localhost/wic/html/transfers/new_wic_files/Server/initTransactionID.php?XDEBUG_SESSION_START=PHPSTORM");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec ($ch);
+        $response = json_decode($server_output);
+        curl_close ($ch);
+    }
+
+}
+
+getWfId();
