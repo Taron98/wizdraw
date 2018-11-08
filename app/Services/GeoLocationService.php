35d983dbb14db3c129464c1bb6cb897e7bec431a
hrach.yeremyants <hrach@worldcomfinance.com>
2018-11-08 10:29:24 +0400

    #change - changed geo location service to Google

--- app/Services/GeoLocationService.php
+++ app/Services/GeoLocationService.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Client;
  */
 class GeoLocationService extends AbstractService
 {
-    const API_URL = 'http://ws.geonames.org/countryCodeJSON?username=wizdraw&';
+    const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC25kdJ5TT-N-Wp7dP0_R68iQgsPn0Qen8&language=en&latlng=';
     /** @var  Client */
     private $guzzleClient;
 
@@ -31,9 +31,21 @@ class GeoLocationService extends AbstractService
      */
     public function get(float $latitude, float $longitude)
     {
-        $url = self::API_URL . 'lat=' . $latitude . '&lng=' . $longitude;
+        $url = self::API_URL . $latitude . ',' . $longitude;
         $response = json_decode($this->guzzleClient->get($url)->getBody(), true);
-        return !empty($response['countryCode']) ? $response['countryCode'] : 'HK';
+        $countryCode = null;
+        foreach ($response['results'] as $result) {
+            if (!isset($result['address_components'])) {
+                continue;
+            }
+            foreach ($result['address_components'] as $address) {
+                if (isset($address['short_name']) && strlen($address['short_name']) == 2) {
+                    $countryCode = $address['short_name'];
+                }
+            }
+        }
+
+        return $countryCode ? $countryCode : 'HK';
     }
 
 }
