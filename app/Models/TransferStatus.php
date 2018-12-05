e55f849bf769c2c029e7f415794362ba3feb137f
hrach,yeremyants <hrach@worldcomfinance.com>
2018-12-05 14:38:08 +0400

    #UP

--- app/Models/TransferStatus.php
+++ app/Models/TransferStatus.php
@@ -49,7 +49,7 @@ class TransferStatus extends AbstractModel
     const STATUS_REQUEST_CANCEL = 'REQUEST CANCEL';
     const STATUS_FOR_VERIFICATION = 'FOR VERIFICATION';
     const STATUS_COMPLETED = 'Completed';
-    const STATUS_WAIT = 'WAIT';
+    const STATUS_WAIT = 'Posted wizdraw';
 
     /**
      * The table associated with the model.
