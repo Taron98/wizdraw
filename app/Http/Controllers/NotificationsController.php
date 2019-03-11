ad1349e936fef170d2e31244486a5fd89733b492
rubina_98 <rubinas@worldcomfinance.com>
2019-03-11 13:16:44 +0400

    created model

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -5,8 +5,7 @@ namespace Wizdraw\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Wizdraw\Http\Requests\NotificationsRequest;
 use Illuminate\Support\Facades\DB;
-
-
+use Wizdraw\Models\ExpoToken;
 
 
 /**
@@ -21,19 +20,13 @@ class NotificationsController extends AbstractController
      *
      * @param NotificationsRequest $request
      *
-     * @return JsonResponse
+     * @return string
      */
     public function token( NotificationsRequest $request
     ): JsonResponse
     {
         $credentials = $request->all();
-        $token = $credentials['expo_token'];
-        $deviceId = $credentials['deviceId'];
-        $res = DB::table('expo_token')->insert([
-            ['expo_token' => $token],
-            ['device_id' => $deviceId],
-        ]);
-        dd($res);
+        ExpoToken::create($credentials);
 
         return $this->respond([
             'success' => true,
---
+++ app/Models/ExpoToken.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wizdraw\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExpoToken extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = "expo_token";
+
+    /**
+     * @var array
+     */
+    protected $fillable = [ 'device_id',  'expo_token'];
+}
