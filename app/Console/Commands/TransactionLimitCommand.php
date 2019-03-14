a03acedaf137905f9260db0f10a03e0c50753b5b
rubina_98 <rubinas@worldcomfinance.com>
2019-03-14 15:14:01 +0400

    changed seven eleven limitation

--- app/Console/Commands/TransactionLimitCommand.php
+++ app/Console/Commands/TransactionLimitCommand.php
@@ -44,7 +44,7 @@ class TransactionLimitCommand extends Command
      */
     public function setTransactionLimit()
     {
-        $limits = [13 => 5000, 90 => 8000, 119 => 30000];
+        $limits = [13 => 5000, 90 => [ 'circkle-k' => 2300, 'seven-eleven' => 4500 ], 119 => 30000];
         $redis = Redis::connection();
         foreach ($limits as $k => $v){
             $redis->lpush(redis_key('origin', $k, 'amountLimits'), $v);
