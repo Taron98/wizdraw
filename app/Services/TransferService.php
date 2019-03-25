0c8484864c2a4485a6cc847f970866c5a582bd59
rubina_98 <rubinas@worldcomfinance.com>
2019-03-25 11:03:58 +0400

    changed request description +supplier

--- app/Services/TransferService.php
+++ app/Services/TransferService.php
@@ -109,7 +109,6 @@ class TransferService extends AbstractService
         } else {
             $transferStatus = TransferStatus::STATUS_PENDING;
         }
-        $attributes['supplier'] = $supplier;
         $initStatus = $this->transferStatusService->findByStatus($transferStatus);
         // todo: change when we'll add new natures
         $defaultNature = $this->natureService->findByNature(Nature::NATURE_SUPPORT_OR_GIFT);
