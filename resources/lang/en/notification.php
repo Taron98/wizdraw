993e0d7db470177de938af586d67d9476a1c7210
stepan.ohanyan <stepano@worldcomfinance.com>
2019-04-08 18:23:02 +0400

    Remove br tags from push notification message

--- resources/lang/en/notification.php
+++ resources/lang/en/notification.php
@@ -6,10 +6,16 @@ return [
     'missing_identity' => 'To send money you need to take a photo of your ID?<br><br>Take a photo',
     'missing_address'  => 'To send money you need to take a photo of your proof of address?<br><br>Take a photo',
     'missing_profile'  => 'To send money you need to take a photo of yourself?<br><br>Take a selfie',
-    'missing_multiple' => 'For regulatory reasons, you still need to provide a few personal details.<br><br>Add missing details',
+    'missing_multiple' => 'For regulatory reasons, you still need to provide a few personal details.
+
+Add missing details',
 
     'transfer_missing_id'      => 'You prepared a transfer to :receiverName. To complete this, you need to supply some ID.<br><br>Complete transfer',
-    'transfer_missing_receipt' => 'Do you have time to take a photo of the receipt for Transaction :transactionNumber? We cannot complete the transfer to :receiverFirstName without it.<br><br>Take a photo',
-    'transfer_aborted' => 'Dear valued Wizdraw customer, please be informed that your transaction number :transactionNumber is now canceled due to its incompletion at a payment location.<br><br>For further information and assistance please do call customer care on :csPhoneNumber',
+    'transfer_missing_receipt' => 'Do you have time to take a photo of the receipt for Transaction :transactionNumber? We cannot complete the transfer to :receiverFirstName without it.
+
+Take a photo',
+    'transfer_aborted' => 'Dear valued Wizdraw customer, please be informed that your transaction number :transactionNumber is now canceled due to its incompletion at a payment location.
+
+For further information and assistance please do call customer care on :csPhoneNumber',
 
 ];
