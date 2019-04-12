d203de299e2978ef703ce403724110526ba3adc0
stepan.ohanyan <stepano@worldcomfinance.com>
2019-04-12 12:54:11 +0400

    test

--- app/Services/TransferService.php
+++ app/Services/TransferService.php
@@ -102,10 +102,10 @@ class TransferService extends AbstractService
             $transferStatus = TransferStatus::STATUS_PENDING_FOR_PAYMENT_AT_CIRCLE_K;
         } elseif ($attributes['payment_agency'] == '7-eleven') {
             $transferStatus = TransferStatus::STATUS_PENDING_FOR_PAYMENT_AT_7_ELEVEN;
-        } elseif ($attributes['payment_agency'] == 'wic-store' && isset($attributes['c_id']) && isset($attributes['sms_code'])) {
+        } elseif ($attributes['payment_agency'] == 'wizdraw-prepaid' && isset($attributes['c_id']) && isset($attributes['sms_code'])) {
             $transferStatus = TransferStatus::STATUS_WAIT;
             unset($attributes['c_id']);
-            unset($attributes['sms_code']);
+//            unset($attributes['sms_code']);
         } elseif ($attributes['payment_agency'] == 'pay-to-agent') {
             $transferStatus = TransferStatus::STATUS_PENDING_FOR_PAYMENT_AT_PAY_TO_AGENT;
         } else {
