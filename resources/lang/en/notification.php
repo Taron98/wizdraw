8974016313ba0a6deae9ca11b10366f2a3df2a38
rubina_98 <rubinas@worldcomfinance.com>
2019-04-15 13:57:51 +0400

    delays

--- resources/lang/en/notification.php
+++ resources/lang/en/notification.php
@@ -2,15 +2,25 @@
 
 return [
 
-    'missing_passport' => 'To send money you need to take a photo of the main page of your passport.<br><br>Take a photo',
-    'missing_identity' => 'To send money you need to take a photo of your ID?<br><br>Take a photo',
-    'missing_address'  => 'To send money you need to take a photo of your proof of address?<br><br>Take a photo',
-    'missing_profile'  => 'To send money you need to take a photo of yourself?<br><br>Take a selfie',
+    'missing_passport' => 'To send money you need to take a photo of the main page of your passport.
+    
+    Take a photo',
+    'missing_identity' => 'To send money you need to take a photo of your ID?
+    
+    Take a photo',
+    'missing_address'  => 'To send money you need to take a photo of your proof of address?
+    
+    Take a photo',
+    'missing_profile'  => 'To send money you need to take a photo of yourself?
+    
+    Take a selfie',
     'missing_multiple' => 'For regulatory reasons, you still need to provide a few personal details.
 
 Add missing details',
 
-    'transfer_missing_id'      => 'You prepared a transfer to :receiverName. To complete this, you need to supply some ID.<br><br>Complete transfer',
+    'transfer_missing_id'      => 'You prepared a transfer to :receiverName. To complete this, you need to supply some ID.
+    
+    Complete transfer',
     'transfer_missing_receipt' => 'Do you have time to take a photo of the receipt for Transaction :transactionNumber? We cannot complete the transfer to :receiverFirstName without it.
 
 Take a photo',
