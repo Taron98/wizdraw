f76dbbfdff6707b8ef530a50161ed6f1e969c088
stepan.ohanyan <stepano@worldcomfinance.com>
2019-04-23 17:12:45 +0400

    Update validation

--- database/migrations/2019_04_18_160219_add_to_transfers_wizdraw_ils_rate_and_wizdraw_ils_exchange_rate.php
+++ database/migrations/2019_04_18_160219_add_to_transfers_wizdraw_ils_rate_and_wizdraw_ils_exchange_rate.php
@@ -14,8 +14,8 @@ class AddToTransfersWizdrawIlsRateAndWizdrawIlsExchangeRate extends Migration
     public function up()
     {
         Schema::table('transfers', function (Blueprint $table){
-            $table->float('ils_exchange_rate')->nullable()->after('commission');
-            $table->float('ils_base_rate')->nullable()->after('commission');
+            $table->float('ils_exchange_rate', 10, 4)->nullable()->after('commission');
+            $table->float('ils_base_rate', 10, 4)->nullable()->after('commission');
         });
     }
 
