c7015f885b5135a0361f9a7734b110835c88ec8b
stepan.ohanyan <stepano@worldcomfinance.com>
2019-06-06 16:47:55 +0400

    Remove ils rates from validation

--- app/Http/Requests/Transfer/TransferCreateRequest.php
+++ app/Http/Requests/Transfer/TransferCreateRequest.php
@@ -69,11 +69,6 @@ class TransferCreateRequest extends AbstractRequest
             // 'natures', current is const
         ];
 
-        $rules = array_merge($rules, [
-            'ilsBaseRate' => 'numeric',
-            'ilsExchangeRate' => 'numeric'
-        ]);
-
         return $rules;
 
 //        return [
