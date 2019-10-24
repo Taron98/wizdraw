f1cdbc6a25178483f1bff3f026566101a7409b14
Robert <roberth@worldcomfinance.com>
2019-10-24 20:35:29 +0400

    about bug fix

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     | we check against this value if he must update his application or not.
     |
     */
-    'version' => '8.1.53',
+    'version' => '8.1.54',
 
     /*
     |--------------------------------------------------------------------------
